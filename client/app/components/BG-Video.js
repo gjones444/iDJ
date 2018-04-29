7f77a2af09db2f15f7292ab960c7e10ed126f0bb
George Jones <jkerr1870@gmail.com>
2018-04-29 15:51:02 -0400

    Update BG-Video.js

--- client/app/components/BG-Video.js
+++ client/app/components/BG-Video.js
@@ -14,8 +14,7 @@ class BGVideo extends Component {
           <div>
           
             <video id="background-video" loop autoPlay>
-                <source src='https://cf-e2.streamablevideo.com/video/mp4/95ev8.mp4?token=1521099418-e6tMx%2Bh8h5LqkVU04qsPyuMN8PGENGDH8PhEeNEaspI%3D' type="video/mp4" />
-                <source src='./bgvid.mp4' type="video/ogg" />
+                <source src='https://streamable.com/6ybth' type="video/mp4" />
             </video>
             
           </div>
